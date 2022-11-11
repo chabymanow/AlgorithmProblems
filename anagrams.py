1a29fd4c814c9fbebbbd7795e0c561b7439823f6
chabymanow <chabymanow@gmail.com>
2022-11-11 11:21:44 +0000

    fresh

---
+++ anagrams.py
@@ -0,0 +1,21 @@
+# This is a possible solution for anagrams
+# Get two strings and return true if both string are anagrams
+# The function cut down the special characters and change a strings to lower case
+import re
+
+def anagram(string_1, string_2):
+    anagrams = False
+    string_1 = re.sub(r"[^a-zA-Z0-9]","",string_1).lower()
+    string_2 = re.sub(r"[^a-zA-Z0-9]","",string_2).lower()
+    if(len(string_1) == len(string_2)):
+        for char in string_1:
+            if char in string_2:
+                anagrams = True
+            else:
+                anagrams = False
+                break
+    else:
+        anagrams = False
+    
+    return anagrams
+print("The two words are anagrams: " + str(anagram("Second!$@#", "Second")))
